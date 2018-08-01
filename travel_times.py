5aaf03308e3a9ae0fb9390d10850bfac66614662
Harrison <hbwhite1996@gmail.com>
2018-08-01 15:16:54 -0500

    added travel_times.py

---
+++ travel_times.py
@@ -0,0 +1,16 @@
+import googlemaps
+from datetime import datetime
+
+gmaps = googlemaps.Client(key='AIzaSyAzlWRJShTKDbxXe1GFrlr7oFFlYmxjxbg')
+
+def get_travel_time(src_lat=18.997739, src_lon=72.841280, dst_lat=18.880253, dst_lon=72.945137):
+	now = datetime.now()
+	directions_result = gmaps.directions("%f, %f" % (src_lat, src_lon),
+	                                     "%f, %f" % (dst_lat, dst_lon),
+	                                     mode="driving",
+	                                     avoid="ferries",
+	                                     departure_time=now
+	                                    )
+
+	print(directions_result[0]['legs'][0]['distance']['text'])
+	print(directions_result[0]['legs'][0]['duration']['text'])
